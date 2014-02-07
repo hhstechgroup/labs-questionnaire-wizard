411373135dd2e6adbae5e43256f387e3cfe3bc96
artem.pylypenko <artem.pylypenko@LAB31.engagepoint-lab.local>
2014-02-07 15:33:46 +0200

    Add methods for WizardForm: searching Pages,  Topics, Questions by ID

--- wizard-ejb/src/main/java/com/engagepoint/labs/wizard/bean/WizardForm.java
+++ wizard-ejb/src/main/java/com/engagepoint/labs/wizard/bean/WizardForm.java
@@ -4,7 +4,9 @@
  */
 package com.engagepoint.labs.wizard.bean;
 
+import com.engagepoint.labs.wizard.questions.WizardQuestion;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -19,7 +21,7 @@ public class WizardForm implements Serializable {
 
     private String id;
     private String formName;
-    private List<WizardPage> pageList;
+    private List<WizardPage> wizardPageList;
 
     public WizardForm() {
     }
@@ -40,12 +42,88 @@ public class WizardForm implements Serializable {
         this.formName = formName;
     }
 
-    public List<WizardPage> getPageList() {
-        return pageList;
+    public List<WizardPage> getWizardPageList() {
+        return wizardPageList;
     }
 
-    public void setPageList(List<WizardPage> pageList) {
-        this.pageList = pageList;
+    public void setWizardPageList(List<WizardPage> wizardPageList) {
+        this.wizardPageList = wizardPageList;
+    }
+
+    /**
+     * Getting WizardPage by id
+     *
+     * @param pageId WizardPage id
+     * @return WizardPage object or null if there is no WizardPage with such id
+     */
+    public WizardPage getWizardPageById(String pageId) {
+        for (WizardPage wizardPage : wizardPageList) {
+            if (pageId.equals(wizardPage.getId())) {
+                return wizardPage;
+            }
+        }
+        return null;
+    }
+
+    public List<WizardTopic> getAllWizarlTopics() {
+        List<WizardTopic> allTopicsInForm = new ArrayList<>();
+        for (WizardPage wizardPage : wizardPageList) {
+            List<WizardTopic> topicList = wizardPage.getTopicList();
+            for (WizardTopic wizardTopic : topicList) {
+                allTopicsInForm.add(wizardTopic);
+            }
+        }
+        return allTopicsInForm;
+    }
+
+    /**
+     * Getting WizardTopic by id
+     *
+     * @param topicId WizardTopic id
+     * @return WizardTopic object or null if there is no WizardTopic with such
+     * id
+     */
+    public WizardTopic getWizardTopicById(String topicId) {
+        List<WizardTopic> allTopics = getAllWizarlTopics();
+        WizardTopic result = null;
+
+        for (WizardTopic wizardTopic : allTopics) {
+            if (topicId.equals(wizardTopic.getId())) {
+                result = wizardTopic;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public List<WizardQuestion> getAllWizardQuestions() {
+        List<WizardTopic> allWizarlTopics = getAllWizarlTopics();
+        List<WizardQuestion> allWizardQuestions = new ArrayList<>();
+        for (WizardTopic wizardTopic : allWizarlTopics) {
+            List<WizardQuestion> wizardQuestionList = wizardTopic.getWizardQuestionList();
+            for (WizardQuestion wizardQuestion : wizardQuestionList) {
+                allWizardQuestions.add(wizardQuestion);
+            }
+        }
+        return allWizardQuestions;
+    }
+
+    /**
+     * Get WizardQuestion by id
+     *
+     * @param QuestionId WizardQuestion id
+     * @return WizardQuestion object if null if there is no WizardQuestion with
+     * such id
+     */
+    public WizardQuestion getWizardQuestionById(String QuestionId) {
+        List<WizardQuestion> allWizardQuestions = getAllWizardQuestions();
+        for (WizardQuestion wizardQuestion : allWizardQuestions) {
+            if (QuestionId.equals(wizardQuestion.getId())) {
+                return wizardQuestion;
+            }
+        }
+        return null;
     }
 
 }
