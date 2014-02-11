b54454b621ea535d10af4e481681d1c684572f91
igor.guzenko <guzenko1992@gmail.com>
2014-02-11 18:58:56 +0200

    add some changes to generator

--- wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/UiComponentGenerator.java
+++ wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/UiComponentGenerator.java
@@ -4,7 +4,10 @@ import com.engagepoint.component.UIDatePicker;
 import com.engagepoint.component.UIEditor;
 import com.engagepoint.component.UIInput;
 import com.engagepoint.labs.wizard.questions.CheckBoxesQuestion;
+import com.engagepoint.labs.wizard.questions.DropDownQuestion;
+import com.engagepoint.labs.wizard.questions.MultipleChoiseQuestion;
 import com.engagepoint.labs.wizard.questions.WizardQuestion;
+import org.primefaces.component.calendar.Calendar;
 import org.primefaces.component.panel.Panel;
 import org.primefaces.component.selectmanycheckbox.SelectManyCheckbox;
 import org.primefaces.component.selectonelistbox.SelectOneListbox;
@@ -39,16 +42,32 @@ public class UIComponentGenerator {
             case CHECKBOX:
                 SelectManyCheckbox checkbox = new SelectManyCheckbox();
                 optionsList = ((CheckBoxesQuestion) wizardQuestion).getOptionsList();
-                checkbox.getChildren().add(getCheckBoxSelectItems(optionsList));
+                checkbox.getChildren().add(getSelectItems(optionsList));
                 questionPanel.getChildren().add(checkbox);
                 break;
             case CHOOSEFROMLIST:
-                questionPanel.getChildren().add(new SelectOneMenu());
+                optionsList = ((DropDownQuestion) wizardQuestion).getOptionsList();
+                SelectOneMenu selectOneMenu = new SelectOneMenu();
+                selectOneMenu.getChildren().add(getSelectItems(optionsList));
+                questionPanel.getChildren().add(selectOneMenu);
                 break;
             case MULTIPLECHOICE:
-                questionPanel.getChildren().add(new SelectOneListbox());
+                optionsList = ((MultipleChoiseQuestion) wizardQuestion).getOptionsList();
+                SelectOneListbox selectOneListbox = new SelectOneListbox();
+                selectOneListbox.getChildren().add(getSelectItems(optionsList));
+                questionPanel.getChildren().add(selectOneListbox);
+                break;
             case DATE:
                 questionPanel.getChildren().add(new UIDatePicker());
+                break;
+            case TIME:
+                Calendar timeCalendar = new Calendar();
+                timeCalendar.setPattern("HH:mm");
+                timeCalendar.setTimeOnly(true);
+                questionPanel.getChildren().add(timeCalendar);
+                break;
+            // todo add: fileupload, grid, range types
+
 
 
 
@@ -57,12 +76,12 @@ public class UIComponentGenerator {
 
     }
 
-    private UISelectItems getCheckBoxSelectItems(List<String> optionsList) {
+    private UISelectItems getSelectItems(List<String> optionsList) {
         SelectItem item;
         UISelectItems selectItems = new UISelectItems();
         List<SelectItem> itemsList = new ArrayList<>();
         for (int i = 0; i < optionsList.size(); i++) {
-            item = new SelectItem((i+1),optionsList.get(i));
+            item = new SelectItem((i + 1), optionsList.get(i));
             itemsList.add(item);
         }
         selectItems.setValue(itemsList);
