a6f881111e7dd26b0128e4ef802c54ae72e28aed
vyacheslav.mysak <vyacheslav.mysak@engagepoint.com>
2014-02-12 15:48:18 +0000

    NEXT button works properly. Removed unused files

---
+++ wizard-ejb/src/main/java/com/engagepoint/labs/wizard/bean/Stub.java
@@ -0,0 +1,20 @@
+package com.engagepoint.labs.wizard.bean;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Stub
+ */
+@Stateless
+@LocalBean
+public class Stub {
+
+    /**
+     * Default constructor. 
+     */
+    public Stub() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
