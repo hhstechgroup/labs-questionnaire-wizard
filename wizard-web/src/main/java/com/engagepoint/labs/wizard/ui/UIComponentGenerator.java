6e733498f5649a776ef17d69e64d894058f56f9c
Ievgen <electro1709@gmail.com>
2014-02-18 15:08:06 +0200

    Added question tooltip

--- wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/UIComponentGenerator.java
+++ wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/UIComponentGenerator.java
@@ -4,6 +4,7 @@ import com.engagepoint.component.UIEditor;
 import com.engagepoint.component.UIInput;
 import com.engagepoint.labs.wizard.questions.*;
 import com.engagepoint.labs.wizard.values.Value;
+import org.primefaces.component.button.Button;
 import org.primefaces.component.calendar.Calendar;
 import org.primefaces.component.datagrid.DataGrid;
 import org.primefaces.component.fileupload.FileUpload;
@@ -83,6 +84,13 @@ public class UIComponentGenerator {
                 break;
         }
         panel.getChildren().add(component);
+
+        Button tooltip = new Button();
+        tooltip.setId("tooltip_" + question.getId());
+        tooltip.setTitle(question.getHelpText());
+        tooltip.setIcon("ui-icon-question");
+        tooltip.setStyleClass("custom");
+        panel.getChildren().add(tooltip);
 
         return panel;
     }
