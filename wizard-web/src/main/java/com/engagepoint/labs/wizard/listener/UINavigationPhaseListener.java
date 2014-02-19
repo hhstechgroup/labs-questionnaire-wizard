4f4773079878164d430955f1300d411c388a6617
Zhuzepe <frooktoza@gmail.com>
2014-02-19 18:05:50 +0200

    Fix problem with "Cannot remove the same component twice" error

--- wizard-web/src/main/java/com/engagepoint/labs/wizard/listener/UINavigationPhaseListener.java
+++ wizard-web/src/main/java/com/engagepoint/labs/wizard/listener/UINavigationPhaseListener.java
@@ -23,54 +23,51 @@ public class UINavigationPhaseListener implements PhaseListener {
 
     @Override
     public void afterPhase(PhaseEvent event) {
-	// TODO Auto-generated method stub
-
-	System.out.println("After phase");
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void beforePhase(PhaseEvent event) {
-	if (modelForController.isNeedRefresh()) {
-	    modelForController.setNeedRefresh(false);
-	    redirectPage();
-	    FacesContext facesContext = event.getFacesContext();
-	    HttpServletResponse response = (HttpServletResponse) facesContext
-		    .getExternalContext().getResponse();
-	    response.addHeader("Pragma", "no-cache");
-	    response.addHeader("Cache-Control", "no-cache");
-	    response.setHeader("Cache-Control", "no-store");
-	    response.addHeader("Cache-Control", "must-revalidate");
-	    response.addHeader("Cache-Control", "max-age=0");
-    }
-	System.out.println("Before phase");
+        if (modelForController.isNeedRefresh()) {
+            modelForController.setNeedRefresh(false);
+            redirectPage();
+            FacesContext facesContext = event.getFacesContext();
+            HttpServletResponse response = (HttpServletResponse) facesContext
+                    .getExternalContext().getResponse();
+            response.addHeader("Pragma", "no-cache");
+            response.addHeader("Cache-Control", "no-cache");
+            response.setHeader("Cache-Control", "no-store");
+            response.addHeader("Cache-Control", "must-revalidate");
+            response.addHeader("Cache-Control", "max-age=0");
+        }
     }
 
     @Override
     public PhaseId getPhaseId() {
-	return PhaseId.RENDER_RESPONSE;
+        return PhaseId.RENDER_RESPONSE;
     }
 
     private void refreshPage() {
-	FacesContext context = FacesContext.getCurrentInstance();
-	String viewId = context.getViewRoot().getViewId();
-	ViewHandler handler = context.getApplication().getViewHandler();
-	UIViewRoot root = handler.createView(context, viewId);
-	root.setViewId(viewId);
-	context.setViewRoot(root);
+        FacesContext context = FacesContext.getCurrentInstance();
+        String viewId = context.getViewRoot().getViewId();
+        ViewHandler handler = context.getApplication().getViewHandler();
+        UIViewRoot root = handler.createView(context, viewId);
+        root.setViewId(viewId);
+        context.setViewRoot(root);
     }
 
     private void redirectPage() {
-	HttpServletRequest origRequest = (HttpServletRequest) FacesContext
-		.getCurrentInstance().getExternalContext().getRequest();
-	ExternalContext externalContext = FacesContext.getCurrentInstance()
-		.getExternalContext();
-	try {
-	    externalContext.redirect(origRequest.getRequestURI());
+        HttpServletRequest origRequest = (HttpServletRequest) FacesContext
+                .getCurrentInstance().getExternalContext().getRequest();
+        ExternalContext externalContext = FacesContext.getCurrentInstance()
+                .getExternalContext();
+        try {
+            externalContext.redirect(origRequest.getRequestURI());
 
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ wizard-web/src/main/java/com/engagepoint/labs/wizard/listener/UIRestoreViewListener.java
@@ -0,0 +1,60 @@
+package com.engagepoint.labs.wizard.listener;
+
+import com.sun.faces.RIConstants;
+import com.sun.faces.renderkit.RenderKitUtils;
+import com.sun.faces.util.ComponentStruct;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.faces.render.ResponseStateManager;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by artem on 19.02.14.
+ */
+public class UIRestoreViewListener implements PhaseListener {
+    private static final String DYNAMIC_ACTIONS_RENDERKIT = "HTML_BASIC";
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        ResponseStateManager rsm = RenderKitUtils.getResponseStateManager(event.getFacesContext(), DYNAMIC_ACTIONS_RENDERKIT);
+        HttpServletRequest httpRequest = (HttpServletRequest) event.getFacesContext().getExternalContext().getRequest();
+
+        Object[] rawState = (Object[]) rsm.getState(event.getFacesContext(), httpRequest.getRequestURI().replaceFirst(httpRequest.getContextPath(), "").split("\\?")[0]);
+        if (rawState == null)
+            return;
+
+        Map<String, Object> state = (Map<String, Object>) rawState[1];
+        if (state == null)
+            return;
+
+        List<Object> savedActions = (List<Object>) state.get(RIConstants.DYNAMIC_ACTIONS);
+        if (savedActions == null)
+            return;
+
+        for (Iterator<Object> iterator = savedActions.iterator(); iterator.hasNext(); ) {
+            Object object = iterator.next();
+            ComponentStruct action = new ComponentStruct();
+            action.restoreState(event.getFacesContext(), object);
+
+            if (ComponentStruct.ADD.equals(action.action))
+                continue;
+
+            if (action.clientId.startsWith("brd"))
+                iterator.remove();
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RESTORE_VIEW;
+    }
+}
