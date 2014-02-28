1774daf530bf549c511a0ce7dc4832ab828c10a0
vyacheslav.mysak <vyacheslav.mysak@engagepoint.com>
2014-02-28 10:05:44 +0000

    fixed date and time bug

--- wizard-ejb/src/main/java/com/engagepoint/labs/wizard/bean/WizardDataModelGenerator.java
+++ wizard-ejb/src/main/java/com/engagepoint/labs/wizard/bean/WizardDataModelGenerator.java
@@ -24,7 +24,7 @@ import java.util.List;
  * @author artem.pylypenko
  */
 public class WizardDataModelGenerator {
-    
+
     public static final String DATE_FORMAT = "dd-MM-yyyy";
     public static final String TIME_FORMAT = "hh:mm";
 
@@ -39,117 +39,137 @@ public class WizardDataModelGenerator {
     }
 
     public WizardDocument getWizardDocument(List<QuestionnaireForms> forms) {
-        wizardDocument = new WizardDocument();
-        wizardDocument.setFormList(getWizardForms(forms));
-        return wizardDocument;
+	wizardDocument = new WizardDocument();
+	wizardDocument.setFormList(getWizardForms(forms));
+	return wizardDocument;
     }
 
     private List<WizardForm> getWizardForms(List<QuestionnaireForms> forms) {
-        topicNumber = 1;
-        wizardFormList = new ArrayList<>();
-        WizardForm wizardForm;
-        for (QuestionnaireForms questionnaireForms : forms) {
-            List<QuestionnaireForm> questionalFormList = questionnaireForms.getQuestionnaireForm();
-            for (QuestionnaireForm questionnaireForm : questionalFormList) {
-                wizardForm = new WizardForm();
-                wizardForm.setFormName(questionnaireForm.getFormName());
-                wizardForm.setId(questionnaireForm.getFormId());
-                wizardForm.setWizardPageList(getWizardPages(questionnaireForm.getPages()));
-                wizardFormList.add(wizardForm);
-            }
-        }
-        return wizardFormList;
+	topicNumber = 1;
+	wizardFormList = new ArrayList<>();
+	WizardForm wizardForm;
+	for (QuestionnaireForms questionnaireForms : forms) {
+	    List<QuestionnaireForm> questionalFormList = questionnaireForms
+		    .getQuestionnaireForm();
+	    for (QuestionnaireForm questionnaireForm : questionalFormList) {
+		wizardForm = new WizardForm();
+		wizardForm.setFormName(questionnaireForm.getFormName());
+		wizardForm.setId(questionnaireForm.getFormId());
+		wizardForm.setWizardPageList(getWizardPages(questionnaireForm
+			.getPages()));
+		wizardFormList.add(wizardForm);
+	    }
+	}
+	return wizardFormList;
     }
 
     private List<WizardPage> getWizardPages(Pages pages) {
-        List<Page> pageList = pages.getPage();
-        wizardPageList = new ArrayList<>();
-        WizardPage wizardPage;
-        for (Page page : pageList) {
-            wizardPage = new WizardPage();
-            wizardPage.setId(page.getPageId());
-            wizardPage.setPageNumber(page.getPageNumber());
-            wizardPage.setTopicList(getWizardQuestionGroups(page.getGroupsOfQuestions()));
-            wizardPageList.add(wizardPage);
-        }
-        return wizardPageList;
+	List<Page> pageList = pages.getPage();
+	wizardPageList = new ArrayList<>();
+	WizardPage wizardPage;
+	for (Page page : pageList) {
+	    wizardPage = new WizardPage();
+	    wizardPage.setId(page.getPageId());
+	    wizardPage.setPageNumber(page.getPageNumber());
+	    wizardPage.setTopicList(getWizardQuestionGroups(page
+		    .getGroupsOfQuestions()));
+	    wizardPageList.add(wizardPage);
+	}
+	return wizardPageList;
     }
 
-    private List<WizardTopic> getWizardQuestionGroups(GroupsOfQuestions questionsGroups) {
-        List<Group> groupList = questionsGroups.getGroup();
-        wizardTopicList = new ArrayList<>();
-        WizardTopic wizardTopic;
-        for (Group group : groupList) {
-            wizardTopic = new WizardTopic();
-            wizardTopic.setId(group.getGroupId());
-            wizardTopic.setGroupTitle(group.getGroupName());
-            wizardTopic.setWizardQuestionList(getWizardQuestions(group.getQuestions()));
-            wizardTopic.setTopicNumber(topicNumber);
-            wizardTopicList.add(wizardTopic);
-            topicNumber++;
-        }
-        return wizardTopicList;
+    private List<WizardTopic> getWizardQuestionGroups(
+	    GroupsOfQuestions questionsGroups) {
+	List<Group> groupList = questionsGroups.getGroup();
+	wizardTopicList = new ArrayList<>();
+	WizardTopic wizardTopic;
+	for (Group group : groupList) {
+	    wizardTopic = new WizardTopic();
+	    wizardTopic.setId(group.getGroupId());
+	    wizardTopic.setGroupTitle(group.getGroupName());
+	    wizardTopic.setWizardQuestionList(getWizardQuestions(group
+		    .getQuestions()));
+	    wizardTopic.setTopicNumber(topicNumber);
+	    wizardTopicList.add(wizardTopic);
+	    topicNumber++;
+	}
+	return wizardTopicList;
     }
 
     private List<WizardQuestion> getWizardQuestions(Questions questions) {
-        List<Question> questionList = questions.getQuestion();
-        wizardQuestionList = new ArrayList<>();
-        for (Question question : questionList) {
-            if (createWizardQuestionFromXmlQuestion(question) != null) {
-                wizardQuestionList.add(createWizardQuestionFromXmlQuestion(question));
-            }
-        }
-        return wizardQuestionList;
+	List<Question> questionList = questions.getQuestion();
+	wizardQuestionList = new ArrayList<>();
+	for (Question question : questionList) {
+	    if (createWizardQuestionFromXmlQuestion(question) != null) {
+		wizardQuestionList
+			.add(createWizardQuestionFromXmlQuestion(question));
+	    }
+	}
+	return wizardQuestionList;
     }
 
-    private WizardQuestion createWizardQuestionFromXmlQuestion(Question xmlQuestion) {
-//        DependentQuestions dependentQuestions = xmlQuestion.getDependentQuestions(); // not supported yet
-        WizardQuestion wizardQuestion = null;
-        if (xmlQuestion.getQuestionType() == null) {
-            return null;
-        }
-        switch (xmlQuestion.getQuestionType()) {
-            case CHECKBOX:
-                CheckBoxesQuestion checkBoxesQuestion = new CheckBoxesQuestion();
-                checkBoxesQuestion.setOptionsList(xmlQuestion.getOptions().getOption());
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        ListTextValue checkboxDefaults = new ListTextValue();
-                        checkboxDefaults.setValue(xmlQuestion.getDefaultAnswers().getDefaultAnswer());
-                        checkBoxesQuestion.setDefaultAnswer(checkboxDefaults);
-                    }
-                }
-                wizardQuestion = checkBoxesQuestion;
-                break;
-
-            case CHOOSEFROMLIST:
-                DropDownQuestion dropDownQuestion = new DropDownQuestion();
-                dropDownQuestion.setOptionsList(xmlQuestion.getOptions().getOption());
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        TextValue chooseFromListDefaults = new TextValue();
-                        chooseFromListDefaults.setValue(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(0));
-                        dropDownQuestion.setDefaultAnswer(chooseFromListDefaults);
-                    }
-                }
-                wizardQuestion = dropDownQuestion;
-                break;
-    	case DATE:
-	    wizardQuestion = new DateQuestion();
-	    Date date=null;
+    private WizardQuestion createWizardQuestionFromXmlQuestion(
+	    Question xmlQuestion) {
+	// DependentQuestions dependentQuestions =
+	// xmlQuestion.getDependentQuestions(); // not supported yet
+	WizardQuestion wizardQuestion = null;
+	if (xmlQuestion.getQuestionType() == null) {
+	    return null;
+	}
+	switch (xmlQuestion.getQuestionType()) {
+	case CHECKBOX:
+	    CheckBoxesQuestion checkBoxesQuestion = new CheckBoxesQuestion();
+	    checkBoxesQuestion.setOptionsList(xmlQuestion.getOptions()
+		    .getOption());
 	    if (xmlQuestion.getDefaultAnswers() != null) {
-		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-		    DateValue dateDefault = new DateValue();
-		    SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
-		    try {
-			date = formatter.parse(xmlQuestion.getDefaultAnswers()
-				.getDefaultAnswer().get(0));
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    ListTextValue checkboxDefaults = new ListTextValue();
+		    checkboxDefaults.setValue(xmlQuestion.getDefaultAnswers()
+			    .getDefaultAnswer());
+		    checkBoxesQuestion.setDefaultAnswer(checkboxDefaults);
+		}
+	    }
+	    wizardQuestion = checkBoxesQuestion;
+	    break;
 
-		    } catch (ParseException e) {
-			e.printStackTrace();
+	case CHOOSEFROMLIST:
+	    DropDownQuestion dropDownQuestion = new DropDownQuestion();
+	    dropDownQuestion.setOptionsList(xmlQuestion.getOptions()
+		    .getOption());
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    TextValue chooseFromListDefaults = new TextValue();
+		    chooseFromListDefaults.setValue(xmlQuestion
+			    .getDefaultAnswers().getDefaultAnswer().get(0));
+		    dropDownQuestion.setDefaultAnswer(chooseFromListDefaults);
+		}
+	    }
+	    wizardQuestion = dropDownQuestion;
+	    break;
+	case DATE:
+	    wizardQuestion = new DateQuestion();
+	    Date date = null;
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			    .get(0).isEmpty()) {
+			DateValue dateDefault = new DateValue();
+			SimpleDateFormat formatter = new SimpleDateFormat(
+				DATE_FORMAT);
+			try {
+			    date = formatter.parse(xmlQuestion
+				    .getDefaultAnswers().getDefaultAnswer()
+				    .get(0));
+
+			} catch (ParseException e) {
+			    e.printStackTrace();
+			}
+			dateDefault.setValue(date);
+			wizardQuestion.setDefaultAnswer(dateDefault);
 		    }
-		    dateDefault.setValue(date);
-		    wizardQuestion.setDefaultAnswer(dateDefault);
 		}
 	    }
 	    break;
@@ -157,87 +177,106 @@ public class WizardDataModelGenerator {
 	    wizardQuestion = new TimeQuestion();
 	    Date time = null;
 	    if (xmlQuestion.getDefaultAnswers() != null) {
-		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-		    DateValue timeDefault = new DateValue();
-		    SimpleDateFormat formatter = new SimpleDateFormat(WizardDataModelGenerator.TIME_FORMAT);
-		    try {
-			time = formatter.parse(xmlQuestion.getDefaultAnswers()
-				.getDefaultAnswer().get(0));
-					    } catch (ParseException e) {
-			e.printStackTrace();
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			    .get(0).isEmpty()) {
+			DateValue timeDefault = new DateValue();
+			SimpleDateFormat formatter = new SimpleDateFormat(
+				WizardDataModelGenerator.TIME_FORMAT);
+			try {
+			    time = formatter.parse(xmlQuestion
+				    .getDefaultAnswers().getDefaultAnswer()
+				    .get(0));
+			} catch (ParseException e) {
+			    e.printStackTrace();
+			}
+			timeDefault.setValue(time);
+			wizardQuestion.setDefaultAnswer(timeDefault);
 		    }
-		    timeDefault.setValue(time);
-		    wizardQuestion.setDefaultAnswer(timeDefault);
 		}
 	    }
 	    break;
-            case FILEUPLOAD:
-                wizardQuestion = new FileUploadQuestion();
-                break;
-            case GRID:
-                GridQuestion gridQuestion = new GridQuestion();
-                gridQuestion.setColumns(xmlQuestion.getGrid().getColumns().getColumn());
-                gridQuestion.setRows(xmlQuestion.getGrid().getRows().getRow());
-                wizardQuestion = gridQuestion;
-                break;
-            case MULTIPLECHOICE:
-                MultipleChoiseQuestion multipleChoiseQuestion = new MultipleChoiseQuestion();
-                multipleChoiseQuestion.setOptionsList(xmlQuestion.getOptions().getOption());
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        TextValue multipleChoiceDefaults = new TextValue();
-                        multipleChoiceDefaults.setValue(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(0));
-                        multipleChoiseQuestion.setDefaultAnswer(multipleChoiceDefaults);
-                    }
-                }
-                wizardQuestion = multipleChoiseQuestion;
-                break;
-            case PARAGRAPHTEXT:
-                wizardQuestion = new TextAreaQuestion();
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        TextValue paragraphDefaults = new TextValue();
-                        paragraphDefaults.setValue(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(0));
-                        wizardQuestion.setDefaultAnswer(paragraphDefaults);
-                    }
-                }
-                break;
-            case RANGE:
-                RangeQuestion rangeQuestion = new RangeQuestion();
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        RangeValue rangeDefaults = new RangeValue();
-                        Range range = new Range();
-                        range.setStart(Integer.parseInt(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(0)));
-                        range.setEnd(Integer.parseInt(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(1)));
-                        rangeDefaults.setValue(range);
-                        rangeQuestion.setDefaultAnswer(rangeDefaults);
-                    }
-                }
-                rangeQuestion.setRange(xmlQuestion.getRange().getRangeBegin(), xmlQuestion.getRange().getRangeEnd());
-                wizardQuestion = rangeQuestion;
-                break;
-            case TEXT:
-                wizardQuestion = new TextQuestion();
-                if (xmlQuestion.getDefaultAnswers() != null) {
-                    if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer().isEmpty()) {
-                        TextValue textDefaults = new TextValue();
-                        textDefaults.setValue(xmlQuestion.getDefaultAnswers().getDefaultAnswer().get(0));
-                        wizardQuestion.setDefaultAnswer(textDefaults);
-                    }
-                }
-                break;
-        }
+	case FILEUPLOAD:
+	    wizardQuestion = new FileUploadQuestion();
+	    break;
+	case GRID:
+	    GridQuestion gridQuestion = new GridQuestion();
+	    gridQuestion.setColumns(xmlQuestion.getGrid().getColumns()
+		    .getColumn());
+	    gridQuestion.setRows(xmlQuestion.getGrid().getRows().getRow());
+	    wizardQuestion = gridQuestion;
+	    break;
+	case MULTIPLECHOICE:
+	    MultipleChoiseQuestion multipleChoiseQuestion = new MultipleChoiseQuestion();
+	    multipleChoiseQuestion.setOptionsList(xmlQuestion.getOptions()
+		    .getOption());
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    TextValue multipleChoiceDefaults = new TextValue();
+		    multipleChoiceDefaults.setValue(xmlQuestion
+			    .getDefaultAnswers().getDefaultAnswer().get(0));
+		    multipleChoiseQuestion
+			    .setDefaultAnswer(multipleChoiceDefaults);
+		}
+	    }
+	    wizardQuestion = multipleChoiseQuestion;
+	    break;
+	case PARAGRAPHTEXT:
+	    wizardQuestion = new TextAreaQuestion();
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    TextValue paragraphDefaults = new TextValue();
+		    paragraphDefaults.setValue(xmlQuestion.getDefaultAnswers()
+			    .getDefaultAnswer().get(0));
+		    wizardQuestion.setDefaultAnswer(paragraphDefaults);
+		}
+	    }
+	    break;
+	case RANGE:
+	    RangeQuestion rangeQuestion = new RangeQuestion();
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    RangeValue rangeDefaults = new RangeValue();
+		    Range range = new Range();
+		    range.setStart(Integer.parseInt(xmlQuestion
+			    .getDefaultAnswers().getDefaultAnswer().get(0)));
+		    range.setEnd(Integer.parseInt(xmlQuestion
+			    .getDefaultAnswers().getDefaultAnswer().get(1)));
+		    rangeDefaults.setValue(range);
+		    rangeQuestion.setDefaultAnswer(rangeDefaults);
+		}
+	    }
+	    rangeQuestion.setRange(xmlQuestion.getRange().getRangeBegin(),
+		    xmlQuestion.getRange().getRangeEnd());
+	    wizardQuestion = rangeQuestion;
+	    break;
+	case TEXT:
+	    wizardQuestion = new TextQuestion();
+	    if (xmlQuestion.getDefaultAnswers() != null) {
+		if (!xmlQuestion.getDefaultAnswers().getDefaultAnswer()
+			.isEmpty()) {
+		    TextValue textDefaults = new TextValue();
+		    textDefaults.setValue(xmlQuestion.getDefaultAnswers()
+			    .getDefaultAnswer().get(0));
+		    wizardQuestion.setDefaultAnswer(textDefaults);
+		}
+	    }
+	    break;
+	}
 
-        if (wizardQuestion != null) {
-            wizardQuestion.setId(xmlQuestion.getQuestionId());
-            wizardQuestion.setTitle(xmlQuestion.getQuestionTitle());
-            wizardQuestion.setQuestionType(xmlQuestion.getQuestionType());
-            wizardQuestion.setHelpText(xmlQuestion.getHelpText());
-            wizardQuestion.setAnswerRequired(xmlQuestion.isAnswerRequired());
-        }
+	if (wizardQuestion != null) {
+	    wizardQuestion.setId(xmlQuestion.getQuestionId());
+	    wizardQuestion.setTitle(xmlQuestion.getQuestionTitle());
+	    wizardQuestion.setQuestionType(xmlQuestion.getQuestionType());
+	    wizardQuestion.setHelpText(xmlQuestion.getHelpText());
+	    wizardQuestion.setAnswerRequired(xmlQuestion.isAnswerRequired());
+	}
 
-        return wizardQuestion;
+	return wizardQuestion;
     }
 
 }
--- wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/validators/QuestionAnswerValidator.java
+++ wizard-web/src/main/java/com/engagepoint/labs/wizard/ui/validators/QuestionAnswerValidator.java
@@ -1,17 +1,12 @@
 package com.engagepoint.labs.wizard.ui.validators;
 
+import java.util.List;
+
 import com.engagepoint.labs.wizard.questions.WizardQuestion;
+import com.engagepoint.labs.wizard.values.DateValue;
 import com.engagepoint.labs.wizard.values.ListTextValue;
 import com.engagepoint.labs.wizard.values.TextValue;
 import com.engagepoint.labs.wizard.values.Value;
-import org.primefaces.component.inputtext.InputText;
-import org.primefaces.component.inputtextarea.InputTextarea;
-
-import javax.faces.component.UIComponent;
-import javax.faces.component.html.HtmlSelectOneMenu;
-import javax.faces.context.FacesContext;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by artem.pylypenko on 2/27/14.
@@ -22,102 +17,125 @@ public class QuestionAnswerValidator {
     private String errorMessage;
 
     public QuestionAnswerValidator(WizardQuestion question) {
-        this.question = question;
+	this.question = question;
     }
 
     public boolean validate(Value value) {
-        switch (question.getQuestionType()) {
-            case TEXT:
-                return validateTextValue(value);
-            case PARAGRAPHTEXT:
-                return validateTextValue(value);
-            case MULTIPLECHOICE:
-                return validateTextValue(value);
-            case CHECKBOX:
-                return validateListTextValue(value);
-            case CHOOSEFROMLIST:
-                return validateTextValue(value);
-        }
-        return true;
+	switch (question.getQuestionType()) {
+	case TEXT:
+	    return validateTextValue(value);
+	case PARAGRAPHTEXT:
+	    return validateTextValue(value);
+	case MULTIPLECHOICE:
+	    return validateTextValue(value);
+	case CHECKBOX:
+	    return validateListTextValue(value);
+	case CHOOSEFROMLIST:
+	    return validateTextValue(value);
+	case DATE:
+	    return validateDateValue(value);
+	case TIME:
+	    return validateTimeValue(value);
+	default:
+	    break;
+	}
+	return true;
     }
 
-
     public boolean validateDropDownQuestionComponent(Object value) {
-        if (value == null || value.toString().isEmpty()) {
-            errorMessage = "Answer must be selected for this question!";
-            return false;
-        }
-        return true;
+	if (value == null || value.toString().isEmpty()) {
+	    errorMessage = "Answer must be selected for this question!";
+	    return false;
+	}
+	return true;
     }
 
     public boolean validateCheckBoxQuestionComponent(Object value) {
-        if (value == null || ((Object[]) value).length == 0) {
-            errorMessage = "Empty field is not allowed here";
-            return false;
-        }
-        return true;
+	if (value == null || ((Object[]) value).length == 0) {
+	    errorMessage = "Empty field is not allowed here";
+	    return false;
+	}
+	return true;
     }
 
     public boolean validateMultipleChoiseQuestionComponent(Object value) {
-        if (value == null || value.toString().isEmpty()) {
-            errorMessage = "Answer must be selected for this question!";
-            return false;
-        }
-        return true;
+	if (value == null || value.toString().isEmpty()) {
+	    errorMessage = "Answer must be selected for this question!";
+	    return false;
+	}
+	return true;
     }
 
     public boolean validateTextAreaQuestionComponent(Object value) {
-        if (value == null) {
-            errorMessage = "Empty field is not allowed here!";
-            return false;
-        } else {
-            String currentValue = value.toString();
-            currentValue = currentValue.replaceAll("\\s", "");
-            if (currentValue.isEmpty()) {
-                errorMessage = "Empty field is not allowed here!";
-                return false;
-            }
-        }
-        return true;
+	if (value == null) {
+	    errorMessage = "Empty field is not allowed here!";
+	    return false;
+	} else {
+	    String currentValue = value.toString();
+	    currentValue = currentValue.replaceAll("\\s", "");
+	    if (currentValue.isEmpty()) {
+		errorMessage = "Empty field is not allowed here!";
+		return false;
+	    }
+	}
+	return true;
     }
 
     public boolean validateTextQuestionComponent(Object value) {
-        if (value == null) {
-            errorMessage = "Empty field is not allowed here!";
-            return false;
-        } else {
-            String currentValue = value.toString();
-            currentValue = currentValue.replaceAll("\\s", "");
-            if (currentValue.isEmpty()) {
-                errorMessage = "Empty field is not allowed here!";
-                return false;
-            }
-        }
-        return true;
+	if (value == null) {
+	    errorMessage = "Empty field is not allowed here!";
+	    return false;
+	} else {
+	    String currentValue = value.toString();
+	    currentValue = currentValue.replaceAll("\\s", "");
+	    if (currentValue.isEmpty()) {
+		errorMessage = "Empty field is not allowed here!";
+		return false;
+	    }
+	}
+	return true;
     }
 
     public String getErrorMessage() {
-        return errorMessage;
+	return errorMessage;
     }
 
     public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
+	this.errorMessage = errorMessage;
     }
 
     private boolean validateTextValue(Value value) {
-        TextValue textValue = (TextValue) value;
-        if (textValue == null || textValue.getValue().toString().isEmpty()) {
-            return false;
-        }
-        return true;
+	TextValue textValue = (TextValue) value;
+	if (textValue == null || textValue.getValue().toString().isEmpty()) {
+	    return false;
+	}
+	return true;
     }
 
     private boolean validateListTextValue(Value value) {
-        ListTextValue listTextValue = (ListTextValue) value;
-        if (listTextValue == null || ((List) listTextValue.getValue()).isEmpty()) {
-            return false;
-        }
-        return true;
+	ListTextValue listTextValue = (ListTextValue) value;
+	if (listTextValue == null
+		|| ((List) listTextValue.getValue()).isEmpty()) {
+	    return false;
+	}
+	return true;
+    }
+
+    private boolean validateDateValue(Value value) {
+	DateValue dateValue = (DateValue) value;
+	if (dateValue == null || dateValue.getValue().toString().isEmpty()) {
+	    return false;
+	}
+	return true;
+
+    }
+
+    private boolean validateTimeValue(Value value) {
+	DateValue timeValue = (DateValue) value;
+	if (timeValue == null || timeValue.getValue().toString().isEmpty()) {
+	    return false;
+	}
+	return true;
     }
 
 }
