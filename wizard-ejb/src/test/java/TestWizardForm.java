b2d400a4a2250615c5b62c9a6f646ae55dc0a85e
igor.guzenko <guzenko1992@gmail.com>
2014-03-04 18:49:33 +0200

    JJJJJJJJJJJ======UNIT

--- wizard-ejb/src/test/java/TestWizardForm.java
+++ wizard-ejb/src/test/java/TestWizardForm.java
@@ -23,21 +23,29 @@ public class TestWizardForm {
         testingForm = new WizardForm();
         testingList = new ArrayList<>(3);
         WizardPage page;
-        ArrayList<WizardTopic> onePageTopicList = new ArrayList<>(1);
-        onePageTopicList.add(new WizardTopic());
+        ArrayList<WizardTopic> onePageTopicList;
+        WizardTopic uniqueOnePageTopic;
+//        onePageTopicList.add(new WizardTopic());
         allTopicsList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             page = new WizardPage();
             page.setId("page " + Integer.toString(i));
-            page.getTopicList();
+            onePageTopicList = new ArrayList<>();
+            for (int j =0;j<3;j++){
+            uniqueOnePageTopic = new WizardTopic();
+            uniqueOnePageTopic.setId(page.getId()+" topic"+j);
+            onePageTopicList.add(uniqueOnePageTopic);
+            }
             page.setTopicList(onePageTopicList);
             testingList.add(i, page);
-
             allTopicsList.addAll(onePageTopicList);
         }
         testingForm.setWizardPageList(testingList);
     }
 
+    /**
+     * test for correct case
+     */
     @Test
     public void testGetWizardPageById() {
         String expectedPageId = "page 1";
@@ -46,16 +54,25 @@ public class TestWizardForm {
         assertEquals(expectedPageId, actualPageId);
     }
 
+    /**
+     * Test for null result
+     */
     @Test
     public void testGetWizardPageA() {
         assertNull(testingForm.getWizardPageById("incorrect id"));
     }
 
+    /**
+     * test for correctness
+     */
     @Test
     public void testGetAllWizardTopics() {
         assertTrue(Arrays.equals(allTopicsList.toArray(), testingForm.getAllWizardTopics().toArray()));
     }
 
+    /**
+     * test for empty topicList in form
+     */
     @Test
     public void testGetAllWizardTopicsA(){
         List pageList = testingForm.getWizardPageList();
@@ -63,5 +80,31 @@ public class TestWizardForm {
             ((WizardPage)page).setTopicList(new ArrayList<WizardTopic>());
         }
         assertTrue(testingForm.getAllWizardTopics().isEmpty());
+    }
+
+    /**
+     * test for topicsIds
+     */
+    @Test
+    public void testGetWizardTopicById(){
+        String expectedTopicId = "page 1 topic1";
+        assertEquals(expectedTopicId,testingForm.getWizardTopicById(expectedTopicId).getId());
+    }
+
+    /**
+     * test for doesn't exists id
+     */
+    @Test
+    public void testGetWizardTopicByIdA(){
+        assertNull(testingForm.getWizardTopicById("notExistingId"));
+    }
+
+    /**
+     * test for finding for correct id
+     */
+    @Test
+    public void testGetWizardTopicByIdB(){
+        String correctId = "page 1 topic2";
+        assertNotNull(testingForm.getWizardTopicById(correctId));
     }
 }
