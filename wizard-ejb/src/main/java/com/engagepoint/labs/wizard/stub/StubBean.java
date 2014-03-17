ac0adbd1b68e113d91c96d9d5f236ae1022964d3
electro1709 <electro1709@gmail.com>
2014-03-17 11:01:35 +0200

    add empty bean

---
+++ wizard-ejb/src/main/java/com/engagepoint/labs/wizard/stub/StubBean.java
@@ -0,0 +1,12 @@
+package com.engagepoint.labs.wizard.stub;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by Ievgen on 17.03.14.
+ */
+@Stateless(name = "StubEJB")
+public class StubBean {
+    public StubBean() {
+    }
+}
